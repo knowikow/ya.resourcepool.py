9bb41c28d9d46a6f5bce53429aeb51b979789782
knowikow <51527099+knowikow@users.noreply.github.com>
2020-08-10 12:54:07 +0200

    Update resourcepool.py (#7)

--- src/ya/resourcepool.py
+++ src/ya/resourcepool.py
@@ -9,7 +9,7 @@ from typing import (Any, Callable, Generator, Generic, Iterable, Optional,
                     TypeVar)
 from weakref import finalize
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = 'ResourcePool ResourcePoolEmpty'.split()
 
 R = TypeVar('R')  # pragma: no mutate
